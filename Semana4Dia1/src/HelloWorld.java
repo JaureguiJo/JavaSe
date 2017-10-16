a9e87380b0761622c9d821096db2481b97be1910
JaureguiJo <jordany_jauregui@outlook.com>
2017-10-16 15:30:22 -0500

    chicharronera

--- Semana4Dia1/src/HelloWorld.java
+++ Semana4Dia1/src/HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola a todos");
+		int sum1 = 8, sum2 = 0, res = 0;
+		res = sum1 + sum2;
+		System.out.println(res);
+		double a = 3, b  = 90, c = 1;
+		
+		//chicharronera
+		double x1 = (-b)+(Math.sqrt(((Math.pow(b, 2)-(4*a*c)))/(2*a))); 
+		double x2 = (-b)-(Math.sqrt(((Math.pow(b, 2)-(4*a*c)))/(2*a))); 
+		System.out.println(x1);
+		System.out.println(x2);
 	}
 
 }
