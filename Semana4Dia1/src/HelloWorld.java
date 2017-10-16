5eee0d5e29929c4faabd256cb63eee40f7ee6afe
JaureguiJo <jordany_jauregui@outlook.com>
2017-10-16 16:37:39 -0500

    condiciones

--- Semana4Dia1/src/HelloWorld.java
+++ Semana4Dia1/src/HelloWorld.java
@@ -14,6 +14,36 @@ public class HelloWorld {
 		double x2 = (-b)-(Math.sqrt(((Math.pow(b, 2)-(4*a*c)))/(2*a))); 
 		System.out.println(x1);
 		System.out.println(x2);
+		String abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		int espacio = 3;
+		
+		String palabra = "parangaricutirimicuaro";
+		for(int i = 0; i < palabra.length(); i++){
+			//System.out.println(palabra.charAt(i));
+			palabra = palabra.replace(palabra.charAt(i), abecedario.charAt(i + espacio));
+		}
+		System.out.println(palabra.length());
+		System.out.println(palabra.substring(2, 4));
+		
+		///////// ARRAYAS
+		
+		String[] meses = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+		String[] semana = {"mon", "tue", "wend", "thu", "fri", "sat", "sun"};
+		
+		System.out.println(meses[0]+" "+meses[10]+" "+meses[6]+" "+meses[11]+" "+meses[2]);
+		System.out.println(semana[0]+" "+semana[4]+" "+semana[1]);
+		
+		boolean bool1 = true;
+		boolean bool2 = false;
+		if(bool1 == bool2){
+			System.out.println("YEs");
+		}
+		else if(bool1 != !bool2){
+			System.out.println("nain");
+		}
+		else if(!bool1 == bool2){
+			System.out.println("Ni modo");
+		}
 	}
 
 }
